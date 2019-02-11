e77075e3a6010440315435b41dfe67c54adc763e
Vũ Văn Tiến <tienvu.061298@gmail.com>
2019-02-11 19:20:26 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ const activities_list = [
 client.on('ready', () => {
     setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); 
-        client.user.setActivity(activities_list[index]); 
+        client.user.setStatus(activities_list[index]); 
     }, 20000); 
 });
 
